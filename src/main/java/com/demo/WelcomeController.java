e562e9cde301a05db70f256aa5f675ad2bc64ce0
Anshaj Maya <amaya@theteamcompanies.com>
2022-09-12 19:23:48 +0530

    added name specification

--- src/main/java/com/demo/WelcomeController.java
+++ src/main/java/com/demo/WelcomeController.java
@@ -1,13 +1,14 @@
 package com.demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
-	@GetMapping("/getMessage")
-	public String getWelcomeMessage() {
-		return "Welcome to Jenkins World......";
+	@GetMapping("/getMessage/{name}")
+	public String getWelcomeMessage(@PathVariable("name") String name) {
+		return "Welcome " + name + " to Jenkins World......";
 	}
 }
