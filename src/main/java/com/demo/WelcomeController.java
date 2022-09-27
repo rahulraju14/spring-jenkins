71cb947009fb8dd608d7acd022ff7ad26eca6ee4
Anshaj Maya <amaya@theteamcompanies.com>
2022-09-27 12:40:21 +0530

    -updated controller

--- src/main/java/com/demo/WelcomeController.java
+++ src/main/java/com/demo/WelcomeController.java
@@ -1,14 +1,13 @@
 package com.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
-	@GetMapping("/getMessage/{name}")
-	public String getWelcomeMessage(@PathVariable("name") String name) {
-		return "Welcome " + name + " to Jenkins World......";
+	@RequestMapping("/")
+	public String getWelcomeMessage() {
+		return "Welcome Rahul to Jenkins World......";
 	}
 }
